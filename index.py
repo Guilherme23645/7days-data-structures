f5037afbed808a238e66d886545609ec30f5f2a8
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-08 11:55:12 -0300

    Testing user interaction

--- index.py
+++ index.py
@@ -14,12 +14,44 @@ class ListaDeCompras:
 
 	def listar_itens(self):
 		for i in range(len(self.itens)):
-			print(self.itens[i],self.quantidades[i])
+			print("\t",self.itens[i],self.quantidades[i])
 
 l = ListaDeCompras()
-l.adicionar_item("Cebola",5)
-l.adicionar_item("Alface",2)
-l.adicionar_item("Tomate",6)
-l.adicionar_item("Chocolate",1)
-l.remover_item("Chocolate")
-l.listar_itens()
+
+x = "S"
+
+while x == "S":
+	acao = input("\n\nQue ação deseja fazer?\n\n\tAdicionar items(A)\n\tRemover items(R)\n\tListar items(L)\n\n\t").strip().upper()
+
+	while acao not in "ARL":
+		print("\n\nCaractere inválido\n\n")
+
+		acao = input("Que ação deseja fazer?\n\n\tAdicionar items(A)\n\tRemover items(R)\n\tListar items(L)").strip().upper()
+
+	if acao == "A":
+		print("\n\nAdicionando item")
+		l.adicionar_item(input("\n\n\tNome do item: "), int(input("\n\tQuantidade: ")))
+	elif acao == "R":
+		if l.itens == []:
+			print("\n\nNão há item a ser removido.")
+		else:
+			print("\n\nRemovendo item")
+			l.remover_item(input("\n\n\tNome do item: "))
+	elif acao == "L":
+		print("\n\n")
+		l.listar_itens()
+
+	x = input("\n\nGostaria de fazer mais uma ação: (S/N) ").strip().upper()
+
+	while x not in "SN":
+		print("\n\nCaractere inválido")
+		x = input("\n\nGostaria de fazer mais uma ação: (S/N) ").strip().upper()
+
+
+
+
+
+
+
+
+
