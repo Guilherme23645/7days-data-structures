05ca7d307e67f09452a5a0ac29921aef2b5d91f7
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-10 18:35:07 -0300

    Adicionei um arquivo python com sintaxe similar à de um JSON para otimizar os testes do arquivo

---
+++ day_three/produtos_data.py
@@ -0,0 +1,12 @@
+produtos = [
+    {"nome": "Arroz", "codigo": "7891234567890", "preco": 5.99, "quantidade": 50},
+    {"nome": "Feijao", "codigo": "7891234567891", "preco": 7.49, "quantidade": 30},
+    {"nome": "Macarrao", "codigo": "7891234567892", "preco": 3.99, "quantidade": 40},
+    {"nome": "Azeite", "codigo": "7891234567893", "preco": 15.90, "quantidade": 20},
+    {"nome": "Sal", "codigo": "7891234567894", "preco": 2.50, "quantidade": 100},
+    {"nome": "Acucar", "codigo": "7891234567895", "preco": 4.20, "quantidade": 60},
+    {"nome": "Cafe", "codigo": "7891234567896", "preco": 12.80, "quantidade": 35},
+    {"nome": "Leite", "codigo": "7891234567897", "preco": 6.10, "quantidade": 80},
+    {"nome": "Farinha", "codigo": "7891234567898", "preco": 3.70, "quantidade": 45},
+    {"nome": "Oleo", "codigo": "7891234567899", "preco": 8.99, "quantidade": 55}
+]
