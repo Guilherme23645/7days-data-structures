39a1dae37bbe6d3ba007df1d4dd3ef8d4c1a44da
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-10 13:48:23 -0300

    Classe Produto adicionada

---
+++ day_three/index.py
@@ -0,0 +1,10 @@
+class Produto:
+	def __init__(self,nome,codigo,preco,quantidade):
+		self.nome = nome
+		self.codigo = codigo
+		self.preco = preco
+		self.quantidade = quantidade
+		self.anterior = Null
+		self.proximo = Null
+
+
