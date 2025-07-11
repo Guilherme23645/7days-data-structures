d10f312b509774b3c6475b2ec6c03cb947254524
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-11 13:47:06 -0300

    Esqueleto adicionado

---
+++ day_four/index.py
@@ -0,0 +1,17 @@
+class Pedido:
+	def __init__(self):
+		pass
+
+class FilaDePedidos:
+	def __init__(self):
+		primeiro = None
+		ultimo = None
+
+	def enfileirar(self):
+		pass
+
+	def desenfileirar(self):
+		pass
+
+	def listar_pedidos(self):
+		pass
