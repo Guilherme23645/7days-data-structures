5c4c4d9d5bd7c9f4b903a510565105aa798499d5
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-11 22:33:49 -0300

    Versao final

--- day_four/index.py
+++ day_four/index.py
@@ -1,17 +1,35 @@
 class Pedido:
-	def __init__(self):
-		pass
+	def __init__(self,numero,cliente,itens,valor):
+		self.numero = numero
+		self.cliente = cliente
+		self.itens = itens
+		self.valor = valor
+		self.proximo = None
 
 class FilaDePedidos:
 	def __init__(self):
-		primeiro = None
-		ultimo = None
+		self.primeiro = None
 
-	def enfileirar(self):
-		pass
+	def enfileirar(self, numero, cliente, itens, valor):
+		pedido = Pedido(numero,cliente,itens,valor)
+
+		if self.primeiro == None:
+			self.primeiro = pedido
+		else:
+			fila = self.primeiro
+			while fila.proximo != None:
+				fila = fila.proximo
+
+			fila.proximo = pedido
 
 	def desenfileirar(self):
-		pass
+		self.primeiro = self.primeiro.proximo
 
 	def listar_pedidos(self):
-		pass
+		fila = self.primeiro
+
+		while fila != None:
+			print(f"Número: {fila.numero}, Cliente: {fila.
+cliente}, Itens: {fila.itens}, Valor: {fila.valor}")
+
+			fila = fila.proximo
